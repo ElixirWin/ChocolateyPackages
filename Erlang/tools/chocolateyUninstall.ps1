560f24097625fd657efc269d1941c395d2419657
Norbert Melzer <timmelzer@gmail.com>
2017-02-18 09:59:45 +0100

    remove created shims

--- Erlang/tools/chocolateyUninstall.ps1
+++ Erlang/tools/chocolateyUninstall.ps1
@@ -7,3 +7,6 @@ start-process -wait "C:\Program Files\erl$erl_version\uninstall.exe"
 #And remove the shim files as well.
 Remove-BinFile "erl" -path "$env:ProgramFiles/erl$erl_version/erts-$erl_version/bin/erl.exe"
 Remove-BinFile "werl" -path "$env:ProgramFiles/erl$erl_version/erts-$erl_version/bin/werl.exe"
+Remove-BinFile "erlc" -path "$env:ProgramFiles/erl$erl_version/erts-$erl_version/bin/erlc.exe"
+Remove-BinFile "escript" -path "$env:ProgramFiles/erl$erl_version/erts-$erl_version/bin/escript.exe"
+Remove-BinFile "dialyzer" -path "$env:ProgramFiles/erl$erl_version/erts-$erl_version/bin/dialyzer.exe"
