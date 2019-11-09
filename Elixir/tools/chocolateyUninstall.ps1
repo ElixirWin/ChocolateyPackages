a31abc4b7a2441c6ce2466973b866f4f5315d0f4
Michael Schmidt <michael@dedesignworks.com>
2019-11-08 19:23:24 -0600

    Update to Elixir 1.9.4-otp22

--- Elixir/tools/chocolateyUninstall.ps1
+++ Elixir/tools/chocolateyUninstall.ps1
@@ -1,11 +1,5 @@
 $package = 'Elixir'
+$version = '1.9.4'
 
-Uninstall-ChocolateyZipPackage $packageName $package $zipFileName 'Precompiled.zip'
-
-#And insure we remove the shortcuts to the batch files as well
-#This is solely present for prior versions of the Elixir CNG package.
-Remove-Item "$env:ChocolateyInstall/bin/iex.bat"
-Remove-Item "$env:ChocolateyInstall/bin/elixir.bat"
-Remove-Item "$env:ChocolateyInstall/bin/elixirc.bat"
-Remove-Item "$env:ChocolateyInstall/bin/mix.bat"
+Uninstall-ChocolateyZipPackage $packageName $package $zipFileName 'v$version-otp-22.zip'
 
